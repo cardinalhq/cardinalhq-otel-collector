e9891283a2e52471a085019ff02ecae66ef3c3b0
Michael Graff <mgraff@cardinalhq.io>
2024-04-27 14:19:53 -0500

    rename type

--- processor/chqdecoratorprocessor/internal/sampler/accumulator/impl.go
+++ processor/chqdecoratorprocessor/internal/sampler/accumulator/impl.go
@@ -2,25 +2,25 @@ package accumulator
 
 import "fmt"
 
-type AggregatorImpl[T int64 | float64] struct {
+type AccumulatorImpl[T int64 | float64] struct {
 	buckets []T
 	count   uint64
 	sum     []T
 }
 
 var (
-	_ Accumulator[float64] = &AggregatorImpl[float64]{}
-	_ Accumulator[int64]   = &AggregatorImpl[int64]{}
+	_ Accumulator[float64] = &AccumulatorImpl[float64]{}
+	_ Accumulator[int64]   = &AccumulatorImpl[int64]{}
 )
 
-func NewAggregatorImpl[T int64 | float64](buckets []T) *AggregatorImpl[T] {
-	return &AggregatorImpl[T]{
+func NewAccumulatorImlp[T int64 | float64](buckets []T) *AccumulatorImpl[T] {
+	return &AccumulatorImpl[T]{
 		buckets: buckets,
 		sum:     make([]T, len(buckets)),
 	}
 }
 
-func (a *AggregatorImpl[T]) Add(value []T) error {
+func (a *AccumulatorImpl[T]) Add(value []T) error {
 	if len(value) != len(a.buckets) {
 		return fmt.Errorf("value length does not match buckets length")
 	}
@@ -31,19 +31,19 @@ func (a *AggregatorImpl[T]) Add(value []T) error {
 	return nil
 }
 
-func (a *AggregatorImpl[T]) Buckets() []T {
+func (a *AccumulatorImpl[T]) Buckets() []T {
 	return a.buckets
 }
 
-func (a *AggregatorImpl[T]) Count() uint64 {
+func (a *AccumulatorImpl[T]) Count() uint64 {
 	return a.count
 }
 
-func (a *AggregatorImpl[T]) Sum() []T {
+func (a *AccumulatorImpl[T]) Sum() []T {
 	return a.sum
 }
 
-func (a *AggregatorImpl[T]) Avg() []T {
+func (a *AccumulatorImpl[T]) Avg() []T {
 	ret := make([]T, len(a.buckets))
 	if a.count == 0 {
 		return ret
--- processor/chqdecoratorprocessor/internal/sampler/accumulator/impl_test.go
+++ processor/chqdecoratorprocessor/internal/sampler/accumulator/impl_test.go
@@ -6,73 +6,73 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewAggregatorImpl(t *testing.T) {
+func TestNewAccumulatorImpl(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
-	assert.NotNil(t, aggregator)
-	assert.Equal(t, buckets, aggregator.buckets)
-	assert.Equal(t, uint64(0), aggregator.count)
-	assert.Equal(t, make([]int64, len(buckets)), aggregator.sum)
+	assert.NotNil(t, accumulator)
+	assert.Equal(t, buckets, accumulator.buckets)
+	assert.Equal(t, uint64(0), accumulator.count)
+	assert.Equal(t, make([]int64, len(buckets)), accumulator.sum)
 }
 
-func TestAggregatorImpl_Add(t *testing.T) {
+func TestAccumulatorImpl_Add(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
 	// Test case 1: Add valid value
 	value := []int64{10, 20, 30, 40, 50}
-	err := aggregator.Add(value)
+	err := accumulator.Add(value)
 	assert.NoError(t, err)
-	err = aggregator.Add([]int64{1, 2, 3, 4, 5})
+	err = accumulator.Add([]int64{1, 2, 3, 4, 5})
 	assert.NoError(t, err)
-	assert.Equal(t, []int64{11, 22, 33, 44, 55}, aggregator.sum)
-	assert.Equal(t, uint64(2), aggregator.count)
+	assert.Equal(t, []int64{11, 22, 33, 44, 55}, accumulator.sum)
+	assert.Equal(t, uint64(2), accumulator.count)
 
 	// Test case 2: Add value with incorrect length
 	value = []int64{10, 20, 30, 40}
-	err = aggregator.Add(value)
+	err = accumulator.Add(value)
 	assert.Error(t, err)
-	assert.Equal(t, []int64{11, 22, 33, 44, 55}, aggregator.sum)
-	assert.Equal(t, uint64(2), aggregator.count)
+	assert.Equal(t, []int64{11, 22, 33, 44, 55}, accumulator.sum)
+	assert.Equal(t, uint64(2), accumulator.count)
 }
 
-func TestAggregatorImpl_Buckets(t *testing.T) {
+func TestAccumulatorImpl_Buckets(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
-	assert.Equal(t, buckets, aggregator.Buckets())
+	assert.Equal(t, buckets, accumulator.Buckets())
 }
 
-func TestAggregatorImpl_Count(t *testing.T) {
+func TestAccumulatorImpl_Count(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
-	assert.Equal(t, uint64(0), aggregator.Count())
+	assert.Equal(t, uint64(0), accumulator.Count())
 
-	aggregator.count = 10
-	assert.Equal(t, uint64(10), aggregator.Count())
+	accumulator.count = 10
+	assert.Equal(t, uint64(10), accumulator.Count())
 }
 
-func TestAggregatorImpl_Sum(t *testing.T) {
+func TestAccumulatorImpl_Sum(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
-	assert.Equal(t, make([]int64, len(buckets)), aggregator.Sum())
+	assert.Equal(t, make([]int64, len(buckets)), accumulator.Sum())
 
-	aggregator.sum = []int64{10, 20, 30, 40, 50}
-	assert.Equal(t, []int64{10, 20, 30, 40, 50}, aggregator.Sum())
+	accumulator.sum = []int64{10, 20, 30, 40, 50}
+	assert.Equal(t, []int64{10, 20, 30, 40, 50}, accumulator.Sum())
 }
 
-func TestAggregatorImpl_Avg(t *testing.T) {
+func TestAccumulatorImpl_Avg(t *testing.T) {
 	buckets := []int64{1, 2, 3, 4, 5}
-	aggregator := NewAggregatorImpl(buckets)
+	accumulator := NewAccumulatorImlp(buckets)
 
-	// Test case 1: Empty aggregator
-	assert.Equal(t, make([]int64, len(buckets)), aggregator.Avg())
+	// Test case 1: Empty accumulator
+	assert.Equal(t, make([]int64, len(buckets)), accumulator.Avg())
 
-	// Test case 2: Non-empty aggregator
-	aggregator.sum = []int64{10, 20, 30, 40, 50}
-	aggregator.count = 5
-	assert.Equal(t, []int64{2, 4, 6, 8, 10}, aggregator.Avg())
+	// Test case 2: Non-empty accumulator
+	accumulator.sum = []int64{10, 20, 30, 40, 50}
+	accumulator.count = 5
+	assert.Equal(t, []int64{2, 4, 6, 8, 10}, accumulator.Avg())
 }

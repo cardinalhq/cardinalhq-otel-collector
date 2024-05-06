5fe432b9d43188c44a785f45ee0598b5181d0a65
Michael Graff <mgraff@cardinalhq.io>
2024-05-06 10:28:36 -0500

    simplify rendering histograms

--- exporter/chqs3exporter/internal/translation/table/metrics.go
+++ exporter/chqs3exporter/internal/translation/table/metrics.go
@@ -19,10 +19,10 @@ import (
 	"maps"
 	"math"
 	"slices"
-	"strconv"
 	"strings"
 	"time"
 
+	"github.com/DataDog/sketches-go/ddsketch"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 
 	"github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqs3exporter/internal/trigram"
@@ -143,22 +143,11 @@ func (l *TableTranslator) toddHistogram(metric pmetric.Metric, baseattrs map[str
 		addAttributes(ret, dp.Attributes(), "metric")
 		ret["_cardinalhq.metric_type"] = metricType
 		ret["_cardinalhq.timestamp"] = dp.Timestamp().AsTime().UnixMilli()
-		values := dp.BucketCounts().AsRaw()
-		bounds := dp.ExplicitBounds().AsRaw()
-		total := uint64(0)
-		for j, v := range values {
-			if v == 0 {
-				continue
-			}
-			index := strconv.Itoa(j)
-			total += v
-			ret["_cardinalhq.count."+index] = float64(v)
-			ret["_cardinalhq.bucket."+index] = bounds[j]
-		}
-		ret["_cardinalhq.value"] = float64(total)
-
+		ret["_cardinalhq.counts"] = dp.BucketCounts().AsRaw()
+		ret["_cardinalhq.bucket_bounds"] = dp.ExplicitBounds().AsRaw()
 		ret["_cardinalhq.name"] = metric.Name()
 		ret["_cardinalhq.id"] = l.idg.Make(time.Now())
+		ret["_cardinalhq.value"] = float64(-1)
 		ensureExpectedKeysMetrics(ret)
 		rets = append(rets, ret)
 	}
@@ -178,33 +167,12 @@ func (l *TableTranslator) toddExponentialHistogram(metric pmetric.Metric, baseat
 		ret["_cardinalhq.metric_type"] = metricType
 		ret["_cardinalhq.timestamp"] = dp.Timestamp().AsTime().UnixMilli()
 		ret["_cardinalhq.scale"] = dp.Scale()
-		total := uint64(0)
-		for j := 0; j < dp.Negative().BucketCounts().Len(); j++ {
-			v := dp.Negative().BucketCounts().At(j)
-			if v == 0 {
-				continue
-			}
-			index := strconv.Itoa(j)
-			total += v
-			ret["_cardinalhq.negative.count."+index] = float64(v)
-		}
-		for j := 0; j < dp.Positive().BucketCounts().Len(); j++ {
-			v := dp.Positive().BucketCounts().At(j)
-			if v == 0 {
-				continue
-			}
-			index := strconv.Itoa(j)
-			total += v
-			ret["_cardinalhq.positive.count."+index] = float64(v)
-		}
-
-		ret["_cardinalhq.zero.count"] = float64(dp.ZeroCount())
-		total += dp.ZeroCount()
-
-		ret["_cardinalhq.value"] = float64(total)
-
+		ret["_cardinalhq.netative.counts"] = dp.Negative().BucketCounts().AsRaw()
+		ret["_cardinalhq.positive.counts"] = dp.Positive().BucketCounts().AsRaw()
+		ret["_cardinalhq.zero.count"] = dp.ZeroCount()
 		ret["_cardinalhq.name"] = metric.Name()
 		ret["_cardinalhq.id"] = l.idg.Make(time.Now())
+		ret["_cardinalhq.value"] = float64(-1)
 		ensureExpectedKeysMetrics(ret)
 		rets = append(rets, ret)
 	}
@@ -217,10 +185,7 @@ func ensureExpectedKeysMetrics(m map[string]any) {
 		"_cardinalhq.rule_id":       "",
 		"_cardinalhq.aggregated_by": "",
 		"_cardinalhq.metric_type":   "gauge",
-		"_cardinalhq.service":       "unknown_service",
-		"_cardinalhq.version":       "",
 		"_cardinalhq.hostname":      findHostname(m),
-		"_cardinalhq.message":       "",
 	}
 
 	for key, val := range keys {
@@ -256,4 +221,50 @@ func valueToString(v any) string {
 		return ""
 	}
 	return fmt.Sprintf("%v", v)
+}
+
+type DDWrapper struct {
+	Sketch         *ddsketch.DDSketch
+	StartTimestamp time.Time
+	Timestamp      time.Time
+	Attributes     map[string]any
+}
+
+func convertToDDSketch(dp pmetric.ExponentialHistogramDataPoint) (*DDWrapper, error) {
+	// Create a new DDSketch with a relative accuracy of 0.01
+	sketch, err := ddsketch.NewDefaultDDSketch(0.01)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the counts from each bucket to the sketch
+	for bucketIndex, count := range dp.Positive().BucketCounts().AsRaw() {
+		// Calculate the bucket value based on the scale and index
+		bucketValue := math.Pow(2, float64(dp.Scale())) * float64(bucketIndex)
+		err := sketch.AddWithCount(bucketValue, float64(count))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for bucketIndex, count := range dp.Negative().BucketCounts().AsRaw() {
+		// Calculate the bucket value based on the scale and index
+		bucketValue := -math.Pow(2, float64(dp.Scale())) * float64(bucketIndex)
+		if err := sketch.AddWithCount(bucketValue, float64(count)); err != nil {
+			return nil, err
+		}
+	}
+
+	// Add the zero count to the sketch
+	if err := sketch.AddWithCount(0, float64(dp.ZeroCount())); err != nil {
+		return nil, err
+	}
+
+	dw := &DDWrapper{
+		Sketch:         sketch,
+		StartTimestamp: dp.StartTimestamp().AsTime(),
+		Timestamp:      dp.Timestamp().AsTime(),
+		Attributes:     dp.Attributes().AsRaw(),
+	}
+	return dw, nil
 }

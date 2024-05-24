3e98c517c89ed159223b20304f8fb0ac3f1bcb41
Michael Graff <mgraff@cardinalhq.io>
2024-05-23 23:28:41 -0700

    config tests

---
+++ exporter/chqstatsexporter/config_test.go
@@ -0,0 +1,87 @@
+// Copyright 2024 CardinalHQ, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package chqstatsexporter
+
+import (
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tj/assert"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configcompression"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configopaque"
+	"go.opentelemetry.io/collector/confmap/confmaptest"
+
+	"github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqstatsexporter/internal/metadata"
+)
+
+func TestLoadConfig(t *testing.T) {
+	t.Parallel()
+
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
+	require.NoError(t, err)
+
+	tests := []struct {
+		id       component.ID
+		expected component.Config
+	}{
+		{
+			id:       component.NewIDWithName(metadata.Type, "default-config"),
+			expected: createDefaultConfig(),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "override-config"),
+			expected: &Config{
+				ClientConfig: confighttp.ClientConfig{
+					Timeout:     500 * time.Millisecond,
+					Endpoint:    "http://localhost:8080",
+					Compression: configcompression.TypeZstd,
+					Headers: map[string]configopaque.String{
+						"Alice":      "Bob",
+						"User-Agent": "cardinalhq-otel-collector",
+					},
+				},
+				Timeboxes: TimeboxesConfig{
+					Logs: TimeboxConfig{
+						Interval: 100 * time.Second,
+					},
+					Metrics: TimeboxConfig{
+						Interval: 200 * time.Second,
+					},
+					Traces: TimeboxConfig{
+						Interval: 300 * time.Second,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id.String(), func(t *testing.T) {
+			factory := NewFactory()
+			cfg := factory.CreateDefaultConfig()
+
+			sub, err := cm.Sub(tt.id.String())
+			require.NoError(t, err)
+			require.NoError(t, component.UnmarshalConfig(sub, cfg))
+
+			assert.NoError(t, component.ValidateConfig(cfg))
+			assert.Equal(t, tt.expected, cfg)
+		})
+	}
+}
--- exporter/chqstatsexporter/go.mod
+++ exporter/chqstatsexporter/go.mod
@@ -3,10 +3,13 @@ module github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqstatsexporter
 go 1.21.0
 
 require (
+	github.com/stretchr/testify v1.9.0
+	github.com/tj/assert v0.0.3
 	go.opentelemetry.io/collector/component v0.101.0
 	go.opentelemetry.io/collector/config/configcompression v1.8.0
 	go.opentelemetry.io/collector/config/confighttp v0.101.0
 	go.opentelemetry.io/collector/config/configopaque v1.8.0
+	go.opentelemetry.io/collector/confmap v0.101.0
 	go.opentelemetry.io/collector/consumer v0.101.0
 	go.opentelemetry.io/collector/exporter v0.100.0
 	go.opentelemetry.io/collector/pdata v1.8.0
@@ -18,6 +21,7 @@ require (
 
 require (
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -35,6 +39,7 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/cors v1.10.1 // indirect
 	go.opentelemetry.io/collector v0.101.0 // indirect
 	go.opentelemetry.io/collector/config/configauth v0.101.0 // indirect
@@ -42,7 +47,6 @@ require (
 	go.opentelemetry.io/collector/config/configtelemetry v0.101.0 // indirect
 	go.opentelemetry.io/collector/config/configtls v0.101.0 // indirect
 	go.opentelemetry.io/collector/config/internal v0.101.0 // indirect
-	go.opentelemetry.io/collector/confmap v0.101.0 // indirect
 	go.opentelemetry.io/collector/extension v0.101.0 // indirect
 	go.opentelemetry.io/collector/extension/auth v0.101.0 // indirect
 	go.opentelemetry.io/collector/featuregate v1.8.0 // indirect
@@ -54,4 +58,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240515191416-fc5f0ca64291 // indirect
 	google.golang.org/grpc v1.64.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )

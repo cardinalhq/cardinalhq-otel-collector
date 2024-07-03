758a429ff3b92b9e405fe4dd3c3df092df451c50
Michael Graff <mgraff@cardinalhq.io>
2024-07-03 12:44:18 -0500

    add simple xid generator test

---
+++ exporter/chqs3exporter/internal/idgen/xid_test.go
@@ -0,0 +1,24 @@
+package idgen
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXidGenerator_Make(t *testing.T) {
+	x := NewXIDGenerator()
+	id := x.Make(time.Now())
+	assert.NotEmpty(t, id, "failed to generate ID")
+}
+
+func TestXidGenerator_Make_many(t *testing.T) {
+	x := NewXIDGenerator()
+	ids := make(map[string]bool)
+	for i := 0; i < 1000; i++ {
+		id := x.Make(time.Now())
+		ids[id] = true
+	}
+	assert.Len(t, ids, 1000, "failed to generate unique IDs")
+}

9491bc70056ef25fc405ba7362d7175280859f97
Michael Graff <mgraff@cardinalhq.io>
2024-08-15 19:10:51 -0500

    add test

---
+++ exporter/chqs3exporter/internal/translation/table/namemap_test.go
@@ -0,0 +1,51 @@
+package table
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSanitizeAttribute(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"attribute_name",
+			"attribute_name",
+		},
+		{
+			"attribute-name",
+			"attribute-name",
+		},
+		{
+			"attribute name",
+			"attribute_name",
+		},
+		{
+			"attribute@name",
+			"attribute_name",
+		},
+		{
+			"_attribute_name",
+			"attribute_name",
+		},
+		{
+			"_____attribute_name",
+			"attribute_name",
+		},
+		{
+			"attribute_name___",
+			"attribute_name",
+		},
+		{
+			"attribute  name",
+			"attribute_name",
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, sanitizeAttribute(test.input))
+	}
+}

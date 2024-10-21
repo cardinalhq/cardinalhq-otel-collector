97acafdb2108b5a08db3a56070bd87fb22e3f597
Michael Graff <mgraff@cardinalhq.io>
2024-10-21 12:52:25 -0500

    fingerprint processor

---
+++ processor/fingerprintprocessor/factory.go
@@ -0,0 +1,67 @@
+// Copyright 2024 CardinalHQ, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fingerprintprocessor
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+
+	"github.com/cardinalhq/cardinalhq-otel-collector/processor/fingerprintprocessor/internal/metadata"
+)
+
+// NewFactory creates a factory for S3 exporter.
+func NewFactory() processor.Factory {
+	return processor.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		processor.WithTraces(createSpansProcessor, metadata.TracesStability),
+		processor.WithLogs(createLogsProcessor, metadata.LogsStability),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		TracesConfig: TracesConfig{
+			EstimatorWindowSize: 30,
+			EstimatorInterval:   10000,
+		},
+	}
+}
+
+func createLogsProcessor(ctx context.Context, set processor.Settings, cfg component.Config, nextConsumer consumer.Logs) (processor.Logs, error) {
+	e, err := newPitbull(cfg.(*Config), "logs", set)
+	if err != nil {
+		return nil, err
+	}
+	return processorhelper.NewLogs(
+		ctx, set, cfg, nextConsumer,
+		e.ConsumeLogs,
+		processorhelper.WithCapabilities(e.Capabilities()))
+}
+
+func createSpansProcessor(ctx context.Context, set processor.Settings, cfg component.Config, nextConsumer consumer.Traces) (processor.Traces, error) {
+	e, err := newPitbull(cfg.(*Config), "traces", set)
+	if err != nil {
+		return nil, err
+	}
+	return processorhelper.NewTraces(
+		ctx, set, cfg, nextConsumer,
+		e.ConsumeTraces,
+		processorhelper.WithCapabilities(e.Capabilities()))
+}
---
+++ processor/fingerprintprocessor/logs.go
@@ -0,0 +1,55 @@
+// Copyright 2024 CardinalHQ, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fingerprintprocessor
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"go.uber.org/zap"
+
+	"github.com/cardinalhq/cardinalhq-otel-collector/internal/translate"
+)
+
+func getServiceName(r pcommon.Map) string {
+	snk := string(semconv.ServiceNameKey)
+	if serviceNameField, found := r.Get(snk); found {
+		return serviceNameField.AsString()
+	}
+	return "unknown"
+}
+
+func (e *pitbull) ConsumeLogs(_ context.Context, ld plog.Logs) (plog.Logs, error) {
+	for i := 0; i < ld.ResourceLogs().Len(); i++ {
+		rl := ld.ResourceLogs().At(i)
+		for j := 0; j < rl.ScopeLogs().Len(); j++ {
+			sl := rl.ScopeLogs().At(j)
+			for k := 0; k < sl.LogRecords().Len(); k++ {
+				lr := sl.LogRecords().At(k)
+				fingerprint, level, err := e.logFingerprinter.Fingerprint(lr.Body().AsString())
+				if err != nil {
+					e.logger.Debug("Error fingerprinting log", zap.Error(err))
+					continue
+				}
+				lr.Attributes().PutInt(translate.CardinalFieldFingerprint, fingerprint)
+				lr.Attributes().PutStr(translate.CardinalFieldLevel, level)
+			}
+		}
+	}
+
+	return ld, nil
+}

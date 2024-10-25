3afe6a4916d61ffc0cc9ab9abacce5bfb4c96357
Michael Graff <explorer@flame.org>
2024-10-25 15:38:06 -0500

    clean up structures

--- internal/ottl/samplerconfig.go
+++ internal/ottl/samplerconfig.go
@@ -14,21 +14,31 @@
 
 package ottl
 
-type ControlPlaneConfig struct {
-	Logs    InstructionConfig `json:"logs,omitempty" yaml:"logs,omitempty"`
-	Metrics InstructionConfig `json:"metrics,omitempty" yaml:"metrics,omitempty"`
-	Spans   InstructionConfig `json:"spans,omitempty" yaml:"spans,omitempty"`
-
-	LogMetricExtractors  []MetricExtractorConfig `json:"log_extractors"`
-	SpanMetricExtractors []MetricExtractorConfig `json:"span_extractors"`
-
-	LogsEnrichments    []StatsEnrichment `json:"logs_enrichments,omitempty" mapstructure:"logs_enrichments"`
-	MetricsEnrichments []StatsEnrichment `json:"metrics_enrichments,omitempty" mapstructure:"metrics_enrichments"`
-	TracesEnrichments  []StatsEnrichment `json:"traces_enrichments,omitempty" mapstructure:"traces_enrichments"`
+type PitbullProcessorConfig struct {
+	LogStatements    []ContextStatement `json:"logs_statements,omitempty"`
+	MetricStatements []ContextStatement `json:"metrics,omitempty" yaml:"metrics,omitempty"`
+	SpanStatements   []ContextStatement `json:"spans,omitempty" yaml:"spans,omitempty"`
 
 	LogsLookupConfigs    []LookupConfig `json:"logs_lookup_configs,omitempty" yaml:"logs_lookup_configs,omitempty"`
 	TracesLookupConfigs  []LookupConfig `json:"spans_lookup_configs,omitempty" yaml:"spans_lookup_configs,omitempty"`
 	MetricsLookupConfigs []LookupConfig `json:"metrics_lookup_configs,omitempty" yaml:"metrics_lookup_configs,omitempty"`
+}
+
+type StatsProcessorConfig struct {
+	LogsEnrichments    []StatsEnrichment `json:"logs_enrichments,omitempty" mapstructure:"logs_enrichments"`
+	MetricsEnrichments []StatsEnrichment `json:"metrics_enrichments,omitempty" mapstructure:"metrics_enrichments"`
+	TracesEnrichments  []StatsEnrichment `json:"traces_enrichments,omitempty" mapstructure:"traces_enrichments"`
+}
+
+type ExtractMetricsProcessorConfig struct {
+	LogMetricExtractors  []MetricExtractorConfig `json:"log_extractors"`
+	SpanMetricExtractors []MetricExtractorConfig `json:"span_extractors"`
+}
+
+type ControlPlaneConfig struct {
+	Pitbulls       map[string]PitbullProcessorConfig        `json:"pitbulls,omitempty" yaml:"pitbulls,omitempty"`
+	Stats          map[string]StatsProcessorConfig          `json:"stats,omitempty" yaml:"stats,omitempty"`
+	ExtractMetrics map[string]ExtractMetricsProcessorConfig `json:"extract_metrics,omitempty" yaml:"extract_metrics,omitempty"`
 
 	hash uint64
 }
@@ -38,21 +48,16 @@ type StatsEnrichment struct {
 	Tags    []string `json:"tags,omitempty" mapstructure:"tags"`
 }
 
-type InstructionConfig struct {
-	Decorators []Instruction `json:"decorators,omitempty" yaml:"decorators,omitempty"`
-}
-
-type ContextID string
-
 type SamplingConfig struct {
 	SampleRate float64 `json:"sampleRate,omitempty" yaml:"sampleRate,omitempty"`
 	RPS        int     `json:"rps,omitempty" yaml:"rps,omitempty"`
 }
 
 type Instruction struct {
-	ProcessorID string             `json:"processorId,omitempty" yaml:"processorId,omitempty"`
-	Statements  []ContextStatement `json:"statements,omitempty" yaml:"statements,omitempty"`
+	Statements []ContextStatement `json:"statements,omitempty" yaml:"statements,omitempty"`
 }
+
+type ContextID string
 
 type ContextStatement struct {
 	Context        ContextID      `json:"context,omitempty" yaml:"context,omitempty"`

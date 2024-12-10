fbbce25c78efa27bf20105d49683db0ecba0e2e8
ruchirj <ruchir.jha@gmail.com>
2024-12-09 17:44:29 -0800

    Disabling stats temporarily

--- processor/chqstatsprocessor/logs.go
+++ processor/chqstatsprocessor/logs.go
@@ -50,29 +50,29 @@ func getFingerprint(l pcommon.Map) int64 {
 }
 
 func (e *statsProc) ConsumeLogs(ctx context.Context, ld plog.Logs) (plog.Logs, error) {
-	var ee translate.Environment
-	if e.idsFromEnv {
-		ee = translate.EnvironmentFromEnv()
-	} else {
-		ee = translate.EnvironmentFromAuth(ctx)
-	}
-
-	now := time.Now()
-
-	for i := 0; i < ld.ResourceLogs().Len(); i++ {
-		rl := ld.ResourceLogs().At(i)
-		serviceName := getServiceName(rl.Resource().Attributes())
-		for j := 0; j < rl.ScopeLogs().Len(); j++ {
-			sl := rl.ScopeLogs().At(j)
-			for k := 0; k < sl.LogRecords().Len(); k++ {
-				lr := sl.LogRecords().At(k)
-				fp := getFingerprint(lr.Attributes())
-				if err := e.recordLog(now, ee, serviceName, fp, rl, sl, lr); err != nil {
-					e.logger.Error("Failed to record log", zap.Error(err))
-				}
-			}
-		}
-	}
+	//var ee translate.Environment
+	//if e.idsFromEnv {
+	//	ee = translate.EnvironmentFromEnv()
+	//} else {
+	//	ee = translate.EnvironmentFromAuth(ctx)
+	//}
+	//
+	//now := time.Now()
+	//
+	//for i := 0; i < ld.ResourceLogs().Len(); i++ {
+	//	rl := ld.ResourceLogs().At(i)
+	//	serviceName := getServiceName(rl.Resource().Attributes())
+	//	for j := 0; j < rl.ScopeLogs().Len(); j++ {
+	//		sl := rl.ScopeLogs().At(j)
+	//		for k := 0; k < sl.LogRecords().Len(); k++ {
+	//			lr := sl.LogRecords().At(k)
+	//			fp := getFingerprint(lr.Attributes())
+	//			if err := e.recordLog(now, ee, serviceName, fp, rl, sl, lr); err != nil {
+	//				e.logger.Error("Failed to record log", zap.Error(err))
+	//			}
+	//		}
+	//	}
+	//}
 
 	return ld, nil
 }

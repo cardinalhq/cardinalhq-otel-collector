0794ebf382c43f75acd7481aa33c85a6d3f97f07
Michael Graff <explorer@flame.org>
2025-01-28 09:37:27 -0600

    disable sanitation of attribute and metric names

--- exporter/chqs3exporter/internal/translation/table/namemap_test.go
+++ exporter/chqs3exporter/internal/translation/table/namemap_test.go
@@ -23,29 +23,50 @@ import (
 func TestSanitizeAttribute(t *testing.T) {
 	tests := []struct {
 		input    string
+		mapNames bool
 		expected string
 	}{
-		{"attribute_name", "attribute_name"},
-		{"attribute-name", "attribute_name"},
-		{"attribute.name", "attribute.name"},
-		{"attribute name", "attribute_name"},
-		{"attribute@name", "attribute_name"},
-		{"_attribute_name", "attribute_name"},
-		{"_____attribute_name", "attribute_name"},
-		{"attribute_name___", "attribute_name"},
-		{"attribute  name", "attribute_name"},
-		{"attribute__name", "attribute_name"},
-		{"Downcases", "downcases"},
-		{"UPPERCASE", "uppercase"},
-		{"AWSCloudWatch.max", "aws_cloud_watch.max"},
-		{"aws.ApplicationELB.UnhealthyStatus", "aws.application_elb.unhealthy_status"},
-		{"AWS.EC2.CPUUtilization", "aws.ec2.cpu_utilization"},
-		{"aws.k8s.cluster.cpuUtilization", "aws.k8s.cluster.cpu_utilization"},
+		{"attribute_name", true, "attribute_name"},
+		{"attribute-name", true, "attribute_name"},
+		{"attribute.name", true, "attribute.name"},
+		{"attribute name", true, "attribute_name"},
+		{"attribute@name", true, "attribute_name"},
+		{"_attribute_name", true, "attribute_name"},
+		{"_____attribute_name", true, "attribute_name"},
+		{"attribute_name___", true, "attribute_name"},
+		{"attribute  name", true, "attribute_name"},
+		{"attribute__name", true, "attribute_name"},
+		{"Downcases", true, "downcases"},
+		{"UPPERCASE", true, "uppercase"},
+		{"AWSCloudWatch.max", true, "aws_cloud_watch.max"},
+		{"aws.ApplicationELB.UnhealthyStatus", true, "aws.application_elb.unhealthy_status"},
+		{"AWS.EC2.CPUUtilization", true, "aws.ec2.cpu_utilization"},
+		{"aws.k8s.cluster.cpuUtilization", true, "aws.k8s.cluster.cpu_utilization"},
+
+		{"attribute_name", false, "attribute_name"},
+		{"attribute-name", false, "attribute-name"},
+		{"attribute.name", false, "attribute.name"},
+		{"attribute name", false, "attribute name"},
+		{"attribute@name", false, "attribute@name"},
+		{"_attribute_name", false, "_attribute_name"},
+		{"_____attribute_name", false, "_____attribute_name"},
+		{"attribute_name___", false, "attribute_name___"},
+		{"attribute  name", false, "attribute  name"},
+		{"attribute__name", false, "attribute__name"},
+		{"Downcases", false, "Downcases"},
+		{"UPPERCASE", false, "UPPERCASE"},
+		{"AWSCloudWatch.max", false, "AWSCloudWatch.max"},
+		{"aws.ApplicationELB.UnhealthyStatus", false, "aws.ApplicationELB.UnhealthyStatus"},
+		{"AWS.EC2.CPUUtilization", false, "AWS.EC2.CPUUtilization"},
+		{"aws.k8s.cluster.cpuUtilization", false, "aws.k8s.cluster.cpuUtilization"},
 	}
 
 	for _, test := range tests {
+		originalSetting := MapNames
 		t.Run(test.input, func(t *testing.T) {
+			MapNames = test.mapNames
 			assert.Equal(t, test.expected, sanitizeAttribute(test.input))
 		})
+		MapNames = originalSetting
 	}
 }

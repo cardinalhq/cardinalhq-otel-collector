9790be64383eea14da1dccef6bdd5a38c8570842
Michael Graff <explorer@flame.org>
2025-03-03 23:20:59 -0600

    add dummy file so benchmarks run

--- internal/doc.go
+++ internal/doc.go
@@ -15,20 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Copyright 2024-2025 CardinalHQ, Inc
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package ddpb
-
-//go:generate protoc --go_out=. --go_opt=paths=source_relative metrics.proto
+// Package internal contains the internal implementation of the processor.
+// Also, without this file, benchmarks can't run for some reason.
+package internal

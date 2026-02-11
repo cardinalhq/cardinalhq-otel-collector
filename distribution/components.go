a24596aa19b3f8246fa0a038e096ea0c091b69a2
Michael Graff <mgraff@cardinalhq.io>
2026-02-10 19:42:17 -0800

    Remove chqdatadogexporter (#264)
    ## Summary
    - Remove chqdatadogexporter component
    
    ## Test plan
    - CI build should pass

--- distribution/components.go
+++ distribution/components.go
@@ -75,7 +75,6 @@ import (
 	tinybirdexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/tinybirdexporter"
 	zipkinexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/zipkinexporter"
 	chqs3exporter "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqs3exporter"
-	chqdatadogexporter "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqdatadogexporter"
 	chqservicegraphexporter "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqservicegraphexporter"
 	chqentitygraphexporter "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqentitygraphexporter"
 	chqk8sentitygraphexporter "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqk8sentitygraphexporter"
@@ -646,7 +645,6 @@ func components() (otelcol.Factories, error) {
 		tinybirdexporter.NewFactory(),
 		zipkinexporter.NewFactory(),
 		chqs3exporter.NewFactory(),
-		chqdatadogexporter.NewFactory(),
 		chqservicegraphexporter.NewFactory(),
 		chqentitygraphexporter.NewFactory(),
 		chqk8sentitygraphexporter.NewFactory(),
@@ -705,7 +703,6 @@ func components() (otelcol.Factories, error) {
 		tinybirdexporter.NewFactory().Type(): "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/tinybirdexporter v0.145.0",
 		zipkinexporter.NewFactory().Type(): "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/zipkinexporter v0.145.0",
 		chqs3exporter.NewFactory().Type(): "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqs3exporter v0.145.0",
-		chqdatadogexporter.NewFactory().Type(): "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqdatadogexporter v0.145.0",
 		chqservicegraphexporter.NewFactory().Type(): "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqservicegraphexporter v0.145.0",
 		chqentitygraphexporter.NewFactory().Type(): "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqentitygraphexporter v0.145.0",
 		chqk8sentitygraphexporter.NewFactory().Type(): "github.com/cardinalhq/cardinalhq-otel-collector/exporter/chqk8sentitygraphexporter v0.145.0",
